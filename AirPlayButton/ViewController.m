120e78952fe41c0f7b42e9339904f19df1de24ba
nullproduction <nullproduction@gmail.com>
2013-10-28 17:34:47 +0400

    Init

--- AirPlayButton/ViewController.m
+++ AirPlayButton/ViewController.m
@@ -1,29 +1,51 @@
 //
 //  ViewController.m
-//  AirPlayButton
-//
-//  Created by mezhevikin on 28.10.13.
-//  Copyright (c) 2013 Mezhevikin. All rights reserved.
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
+#import <MediaPlayer/MediaPlayer.h>
 
 @implementation ViewController
 
 - (void)viewDidLoad
 {
+    self.view.backgroundColor = [UIColor grayColor];
+    [self addAirPlayButton];
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
+- (void)addAirPlayButton
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Зеленая вью - контейнер
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(100, 100, 50, 50)];
+    view.backgroundColor = [UIColor greenColor];
+    
+    // MPVolumeView - черная
+    MPVolumeView *volumeView = [[MPVolumeView alloc] initWithFrame:CGRectZero];
+    volumeView.backgroundColor = [UIColor blackColor];
+    // показываем кнопку airplay и скрываем слайдер громкости
+    volumeView.showsRouteButton = YES;
+    volumeView.showsVolumeSlider = NO;
+    // Устанавливаем свою иконку на аирплей кнопку
+    [volumeView setRouteButtonImage:[UIImage imageNamed:@"Airplay.png"] forState:UIControlStateNormal];
+
+    // находим кнопку в subviews
+    for (id view in volumeView.subviews)
+    {
+        if ([[[view class] description] isEqualToString:@"MPButton"])
+        {
+            // делаем кнопку оранжевой и пытаемся сделать отступ от родительского зеленого вью
+            // размеры кнопки изменились а вот координаты x и y как будто не работают
+            UIButton *airPlayButton = (UIButton *)view;
+            airPlayButton.frame = CGRectMake(20, 20, 20, 20);
+            airPlayButton.backgroundColor = [UIColor orangeColor];
+        }
+    }
+    
+    [view addSubview:volumeView];
+    [self.view addSubview:view];
 }
+
+
 
 @end
